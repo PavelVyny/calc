0e8627a1d57609b9ed7b9291c68c1a6b039ca9c5
Pavel Vynychenko <pavel.vynych@gmail.com>
2020-07-16 17:46:12 +0300

    added some basic scss styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,10 @@ class Button extends Component {
 
 	render() {
 		return (
-			<div>
-				<button className='calc-button'>1</button>
+			<div className={`column column_${this.props.cols}`}>
+				<button className='calc__button' onClick={() => this.props.action(this.props.symbol)}>
+					{this.props.symbol}
+				</button>
 			</div>
 		)
 	}
